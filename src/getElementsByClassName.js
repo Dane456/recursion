48063faf8be12e3a71456f4ed5e5d958cb8e59a7
Dane Jordan <dane.e.jordan@gmail.com>
2016-05-21 19:49:59 -0700

    first commit, getElementsByClassName working for top layer divs

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,35 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
+
   // your code here
+  
+var getElementsByClassName = function(className) {
+  
+  // your code here
+  var results = [];
+
+  var getChildElements = function(element){
+
+    var childElement;
+    console.dir(element.childElementCount);
+    if(element.childElementCount === 0 && element.className === className){
+      return element;
+    }
+    //if any children have the classname, dig deeper
+    else if(element.childElementCount > 0){    
+      for(i=0;i<element.childElementCount;i++){
+          if(element.children[i].className === className)
+            results.push(element.children[i]);
+            getChildElements(element.children[i]);
+        }
+    }
+
+  };
+
+  results.push(getChildElements(document.body));
+
+  return results;
+};
 };
